07e835f54f2b1c6c973a728b5d74dec4dc43cb62
ProjektKris <rbxkrisxyz@gmail.com>
2021-07-08 15:43:16 +0700

    added find.js with FindNext function

---
+++ modules/find.js
@@ -0,0 +1,26 @@
+
+module.exports.FindEmpty = (arr) => {
+    /*
+    Returns an index that is empty in `arr` array.
+    If there are no empty indexes, it will return arr.length
+    */
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == null) {
+            return i;
+        }
+    }
+    return arr.length;
+};
+
+module.exports.FindNext = (arr, currentIndex) => {
+    for (let i = currentIndex + 1; i < arr.length; i++) {
+        if (arr[i] != null) {
+            return i;
+        }
+    }
+    for (let i = currentIndex - 1; i >= 0; i++) {
+        if (arr[i] != null) {
+            return i;
+        }
+    }
+};
